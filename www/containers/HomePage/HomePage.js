95812979d99e24c0250885bc3a7f05c0c87d73aa
Wesley Hall <wesleyahall@gmail.com>
2016-07-19 09:56:10 -0500

    iOS vh bugfix (!hackish!)

--- www/containers/HomePage/HomePage.js
+++ www/containers/HomePage/HomePage.js
@@ -4,12 +4,20 @@ import './HomePage.styl'
 
 class HomePage extends React.Component {
   render () {
+    // bug in older versions of safari's web view makes viewport height change over time
+    // so we need to set the menu links height with javascript here. :(
+    const menuItemHeight = document.documentElement.clientHeight * 0.18
+    const menuItemMarginBottom = document.documentElement.clientHeight * 0.02
+    const menuItemStyles = {
+      marginBottom: menuItemMarginBottom,
+      height: menuItemHeight
+    }
     return (
       <div className="Page HomePage">
         <h1>I want to find a</h1>
         <nav className="menu">
           <Link to="/doctors" className="menu__link" activeClassName="menu__link--active">
-            <div className="menu__link__item menu__link__item--doctors">
+            <div className="menu__link__item menu__link__item--doctors" style={menuItemStyles}>
               <div className="menu__link__item__container">
                 <div className="menu__link__item__icon">a</div>
                 <div className="menu__link__item__title">Doctor</div>
@@ -18,7 +26,7 @@ class HomePage extends React.Component {
           </Link>
 
           <Link to="/hospitals" className="menu__link" activeClassName="menu__link--active">
-            <div className="menu__link__item menu__link__item--hospitals">
+            <div className="menu__link__item menu__link__item--hospitals" style={menuItemStyles}>
               <div className="menu__link__item__container">
                 <div className="menu__link__item__icon">b</div>
                 <div className="menu__link__item__title">Hospital</div>
@@ -27,7 +35,7 @@ class HomePage extends React.Component {
           </Link>
 
           <Link to="/pharmacies" className="menu__link" activeClassName="menu__link--active">
-            <div className="menu__link__item menu__link__item--pharmacies">
+            <div className="menu__link__item menu__link__item--pharmacies" style={menuItemStyles}>
               <div className="menu__link__item__container">
                 <div className="menu__link__item__icon">c</div>
                 <div className="menu__link__item__title">Pharmacy</div>
