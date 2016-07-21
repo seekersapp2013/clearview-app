50bf275c50475ca4ba7ed53b61f56d797c7bb0ad
Wesley Hall <wesleyahall@gmail.com>
2016-07-21 11:14:59 -0500

    fixes height issues with csstransistiongroup on non App pages

--- www/containers/AppointmentPage/AppointmentPage.js
+++ www/containers/AppointmentPage/AppointmentPage.js
@@ -5,37 +5,39 @@ import './AppointmentPage.styl'
 import '../App/App.styl'
 
 class AppointmentPage extends React.Component {
-  componentDidMount () {
-    // <a href="mailto:?subject=subject of the email&body=whatever body body" target="_blank">send email</a>
-  }
-
   render () {
+    const footerHeight = 50
+    const backLinkHeight = 30
+    const mainHeight = document.documentElement.clientHeight - (footerHeight)
+
     return (
       <div className="Page AppointmentPage">
-        <header>
-          <BackLink to="/" text="Home" />
-          <div className="AppointmentPage__Icon"></div>
-          <div className="AppointmentPage__Instructions">
-            Need to make an appointment with Clearview?
-            <br /> <br />
-            Input your information below and a representative will be in touch.
-          </div>
-        </header>
-        <main>
-          <div className="AppointmentPage__InputGroup">
-            <label htmlFor="name">My name</label>
-            <input type="text" id="name" name="name" placeholder="Enter your name" required="required" />
-          </div>
-          <div className="AppointmentPage__InputGroup">
-            <label htmlFor="phone">My phone number</label>
-            <input type="number" id="phone" name="phone" placeholder="(555) 555-5555" />
-          </div>
-          <div className="AppointmentPage__InputGroup">
-            <label htmlFor="email">My email address</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email address" />
-          </div>
-          <button>Submit</button>
-        </main>
+        <BackLink to="/" text="Home" />
+        <div style={{height: mainHeight, overflow: 'scroll', paddingBottom: 30}}>
+          <header>
+            <div className="AppointmentPage__Icon"></div>
+            <div className="AppointmentPage__Instructions">
+              Need to make an appointment with Clearview?
+              <br /> <br />
+              Input your information below and a representative will be in touch.
+            </div>
+          </header>
+          <main>
+            <div className="AppointmentPage__InputGroup">
+              <label htmlFor="name">My name</label>
+              <input type="text" id="name" name="name" placeholder="Enter your name" required="required" />
+            </div>
+            <div className="AppointmentPage__InputGroup">
+              <label htmlFor="phone">My phone number</label>
+              <input type="number" id="phone" name="phone" placeholder="(555) 555-5555" />
+            </div>
+            <div className="AppointmentPage__InputGroup">
+              <label htmlFor="email">My email address</label>
+              <input type="email" id="email" name="email" placeholder="Enter your email address" />
+            </div>
+            <button>Submit</button>
+          </main>
+        </div>
         <FooterNav />
       </div>
     )
--- www/containers/ErrorReportPage/ErrorReportPage.js
+++ www/containers/ErrorReportPage/ErrorReportPage.js
@@ -5,31 +5,37 @@ import './ErrorReportPage.styl'
 
 class ErrorReportPage extends React.Component {
   render () {
+    const footerHeight = 50
+    const backLinkHeight = 30
+    const mainHeight = document.documentElement.clientHeight - (footerHeight)
+
     return (
       <div className="Page ErrorReportPage">
-        <header>
-          <BackLink to="/" text="Back" />
-          <div className="ErrorReportPage__Icon"></div>
-          <div className="ErrorReportPage__Instructions">
-            Do we have information that is out of date? <br /> <br />
-            Leave your name, email address, and phone number and a representative will be in touch.
-          </div>
-        </header>
-        <main>
-          <div className="ErrorReportPage__InputGroup">
-            <label htmlFor="name">My name</label>
-            <input type="text" id="name" name="name" placeholder="Enter your name" required="required" />
-          </div>
-          <div className="ErrorReportPage__InputGroup">
-            <label htmlFor="phone">My phone number</label>
-            <input type="tel" id="phone" name="phone" placeholder="(555) 555-5555" />
-          </div>
-          <div className="ErrorReportPage__InputGroup">
-            <label htmlFor="phone">My email address</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email address" />
-          </div>
-          <button>Submit</button>
-        </main>
+        <BackLink to="/" text="Back" />
+        <div style={{height: mainHeight, overflow: 'scroll', paddingBottom: 30}}>
+          <header>
+            <div className="ErrorReportPage__Icon"></div>
+            <div className="ErrorReportPage__Instructions">
+              Do we have information that is out of date? <br /> <br />
+              Leave your name, email address, and phone number and a representative will be in touch.
+            </div>
+          </header>
+          <main>
+            <div className="ErrorReportPage__InputGroup">
+              <label htmlFor="name">My name</label>
+              <input type="text" id="name" name="name" placeholder="Enter your name" required="required" />
+            </div>
+            <div className="ErrorReportPage__InputGroup">
+              <label htmlFor="phone">My phone number</label>
+              <input type="tel" id="phone" name="phone" placeholder="(555) 555-5555" />
+            </div>
+            <div className="ErrorReportPage__InputGroup">
+              <label htmlFor="phone">My email address</label>
+              <input type="email" id="email" name="email" placeholder="Enter your email address" />
+            </div>
+            <button>Submit</button>
+          </main>
+        </div>
         <FooterNav />
       </div>
     )
