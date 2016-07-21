e585ee84b0435899e707ae5fa2eac40890642a76
Wesley Hall <wesleyahall@gmail.com>
2016-07-21 10:32:26 -0500

    clean up header sizing & spacing

--- www/containers/DirectoryPage/DirectoryPage.js
+++ www/containers/DirectoryPage/DirectoryPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import BackLink from '../../components/BackLink'
 import Axios from 'axios'
 import DebouncedInput from 'react-debounce-input'
+import BackLink from '../../components/BackLink'
 import DirectoryList from '../../components/DirectoryList'
 import './DirectoryPage.styl'
 
