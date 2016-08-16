3723f0011c24c4034600c0f79160f656e37a9b02
Wesley Hall <wesleyahall@gmail.com>
2016-08-16 09:42:37 -0500

    listing styles

--- www/containers/App/App.js
+++ www/containers/App/App.js
@@ -3,7 +3,6 @@ import React from 'react'
 import FooterNav from '../../components/FooterNav'
 
 class App extends React.Component {
-
   render () {
     const { children } = this.props
     return (
